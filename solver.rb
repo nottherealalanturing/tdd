a971f48df5297a1131d9fdfed9546e27a805c2ec
Assad Isah <nottherealalanturing@gmail.com>
2022-07-30 01:46:27 +0100

    fix: resolve linter errors with rubocop

--- solver.rb
+++ solver.rb
@@ -1,17 +1,17 @@
 class Solver
-    def factorial(num)
-        fact = 1
-        return 1 if num.zero?
-        raise 'No negative factorial' if num.negative?
+  def factorial(num)
+    fact = 1
+    return 1 if num.zero?
+    raise 'No negative factorial' if num.negative?
 
-        if num.positive?
-        i = 1
-        while i <= num
-            fact *= i
-            i += 1
-        end
-        end
-        fact
+    if num.positive?
+      i = 1
+      while i <= num
+        fact *= i
+        i += 1
+      end
+    end
+    fact
   end
 
   def reverse(word)
