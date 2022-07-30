9a16f5bc958abd0eabd71e02d0d31129c3383007
Assad Isah <nottherealalanturing@gmail.com>
2022-07-30 01:33:50 +0100

    feat: add a method that calculates factorial

---
+++ solver.rb
@@ -0,0 +1,16 @@
+class Solver
+    def factorial(num)
+        fact = 1
+        return 1 if num.zero?
+        raise 'No negative factorial' if num.negative?
+
+        if num.positive?
+        i = 1
+        while i <= num
+            fact *= i
+            i += 1
+        end
+        end
+        fact
+  end
+end
